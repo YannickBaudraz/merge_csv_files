c8f3e3ff5bb7bfa05fcc35511d4dae5fcda27383
ybaudraz <yannick.baudraz@swissquote.ch>
2023-12-27 12:03:49 +0100

    Refactor to have methods

--- merge_csv_files/main.py
+++ merge_csv_files/main.py
@@ -3,27 +3,27 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-log_format = logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-file_handler = RotatingFileHandler('app.log', maxBytes=5000000, backupCount=5)
-file_handler.setFormatter(log_format)
-logger.addHandler(file_handler)
+def setup_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    log_format = logging.Formatter(
+        '%(asctime)s - [%(levelname)s] - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(log_format)
-logger.addHandler(console_handler)
+    file_handler = RotatingFileHandler(
+        'app.log', maxBytes=5000000, backupCount=5)
+    file_handler.setFormatter(log_format)
+    logger.addHandler(file_handler)
 
-try:
-    data_dir_path = os.path.dirname(os.path.realpath(__file__)) + "/data"
-    input_path = data_dir_path + "/in"
-    logging.info(f"Input path set to {input_path}")
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_format)
+    logger.addHandler(console_handler)
 
-    files = [f for f in os.listdir(input_path) if os.path.isfile(input_path + '/' + f)]
+
+def merge_csv_files(input_path):
+    files = [f for f in os.listdir(input_path) if os.path.isfile(
+        os.path.join(input_path, f))]
     logging.info(f"Found {len(files)} files in input directory.")
-
-    df = pd.DataFrame()
 
     data_frames = []
     for file_name in files:
@@ -31,16 +31,38 @@ try:
             logging.warning(f"Skipped non-CSV file: {file_name}")
             continue
 
-        data = pd.read_csv(input_path + '/' + file_name, header=None, dtype=str)
+        data = pd.read_csv(os.path.join(
+            input_path, file_name), header=None, dtype=str)
         logging.info(f"Read data from {file_name} with {len(data)} rows.")
         data_frames.append(data)
 
-    df = pd.concat(data_frames, ignore_index=True)
-    logging.info("Merged all csv files.")
+    return pd.concat(data_frames, ignore_index=True)
 
-    output_path = data_dir_path + '/out/' + str(pd.Timestamp.now().strftime("%Y-%m-%d_%H-%M-%S")) + '.csv'
-    df.to_csv(output_path, index=False, header=False)
-    logging.info(f"Data saved to {output_path}.")
 
-except Exception as e:
-    logging.error("An error occurred", exc_info=True)
+def save_csv(df: pd.DataFrame, output: str):
+    df.to_csv(output, index=False, header=False)
+    logging.info(f"Data saved to {output}.")
+
+
+def main():
+    try:
+        setup_logger()
+        data_dir_path = os.path.dirname(os.path.realpath(__file__)) + "/data"
+        input_path = os.path.join(data_dir_path, "in")
+        logging.info(f"Input path set to {input_path}")
+
+        save_csv(
+            merge_csv_files(input_path),
+            os.path.join(
+                data_dir_path,
+                'out',
+                pd.Timestamp.now().strftime("%Y-%m-%d_%H-%M-%S") + '.csv'
+            )
+        )
+
+    except Exception as e:
+        logging.error("An error occurred", exc_info=True)
+
+
+if __name__ == "__main__":
+    main()
